84e71c5a3d324011dbb084616fda1fe798353da6
Manabendra Sarmah <manabendra.hybrutos@gmail.com>
2022-08-30 22:22:28 +0530

    security update

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ class App extends Component {
   componentDidMount(){
     fetch('http://localhost:3000')
     .then(response => response.json())
-    .then(console.log)
+    //.then(console.log)
   }
 
   loadUser = (data) => {
