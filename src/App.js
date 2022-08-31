8900fe5c43452dbede48800efa1c35dda4ddf9b2
Manabendra Sarmah <manabendra.hybrutos@gmail.com>
2022-08-31 14:30:42 +0530

    url changes

--- src/App.js
+++ src/App.js
@@ -126,7 +126,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://face-recog-back.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
